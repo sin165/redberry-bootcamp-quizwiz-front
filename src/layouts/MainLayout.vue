d5cd858901694448b2988388b4167cb70041288f
Tamar Sinauridze <sin165165@gmail.com>
2024-03-21 00:41:24 +0400

    feat: add footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,14 +3,17 @@
   <div class="pt-18">
     <slot />
   </div>
+  <MainFooter />
 </template>
 
 <script>
 import MainHeader from '@/components/layouts/main/MainHeader.vue'
+import MainFooter from '@/components/layouts/main/MainFooter.vue'
 
 export default {
   components: {
-    MainHeader
+    MainHeader,
+    MainFooter
   }
 }
 </script>
