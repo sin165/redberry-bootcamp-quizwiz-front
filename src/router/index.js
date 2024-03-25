6a26cfd29a0f178c9e68a183b633659d3932e5b5
Tamar Sinauridze <sin165165@gmail.com>
2024-03-25 14:27:50 +0400

    chore: modify component paths in the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue'
+import HomeView from '@/views/HomeView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
