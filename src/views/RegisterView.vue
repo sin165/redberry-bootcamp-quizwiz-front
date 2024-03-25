8de43e94cdee5e41b762d647c601017d3401c866
Tamar Sinauridze <sin165165@gmail.com>
2024-03-25 20:07:16 +0400

    feat: add register form fields

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -25,7 +25,75 @@
             <ValidationError>{{ errors[0] }}</ValidationError>
           </Field>
         </div>
-        <button>Sign Up</button>
+        <div>
+          <BaseLabel for="email">Email</BaseLabel>
+          <Field name="email" rules="required" v-slot="{ field, errors }">
+            <BaseInput
+              id="email"
+              type="email"
+              placeholder="Example@gmail.com"
+              v-bind="field"
+              :isInvalid="!!errors[0]"
+            />
+            <ValidationError>{{ errors[0] }}</ValidationError>
+          </Field>
+        </div>
+        <div>
+          <BaseLabel for="password">Create a password</BaseLabel>
+          <Field name="password" rules="required" v-slot="{ field, errors }">
+            <BaseInput
+              id="password"
+              type="password"
+              placeholder="must be 8 characters"
+              v-bind="field"
+              :isInvalid="!!errors[0]"
+            />
+            <ValidationError>{{ errors[0] }}</ValidationError>
+          </Field>
+        </div>
+        <div>
+          <BaseLabel for="confirm-password">Confirm password</BaseLabel>
+          <Field name="confirm-password" rules="required" v-slot="{ field, errors }">
+            <BaseInput
+              id="confirm-password"
+              type="password"
+              placeholder="must be 8 characters"
+              v-bind="field"
+              :isInvalid="!!errors[0]"
+            />
+            <ValidationError>{{ errors[0] }}</ValidationError>
+          </Field>
+        </div>
+        <div class="pt-2">
+          <Field
+            v-slot="{ field }"
+            name="terms"
+            type="checkbox"
+            :value="true"
+            :unchecked-value="false"
+          >
+            <label class="flex gap-3">
+              <input
+                type="checkbox"
+                name="terms"
+                v-bind="field"
+                :value="true"
+                class="peer hidden"
+              />
+              <div
+                class="size-5 rounded-full border border-custom-gray-400 *:hidden peer-checked:border-none peer-checked:*:block"
+              >
+                <IconChecked />
+              </div>
+              <span class="text-custom-gray-700 text-sm"
+                >I accept the terms and privacy policy</span
+              >
+            </label>
+          </Field>
+        </div>
+        <button class="block w-full h-14 bg-black text-white font-semibold rounded-0.5xl">
+          Sign Up
+        </button>
       </Form>
     </template>
   </AuthLayout>
@@ -35,6 +103,7 @@
 import { Form, Field } from 'vee-validate'
 import AuthLayout from '@/layouts/AuthLayout.vue'
 import IconArtRegister from '@/components/icons/IconArtRegister.vue'
+import IconChecked from '@/components/icons/IconChecked.vue'
 import BaseLabel from '@/components/ui/BaseLabel.vue'
 import BaseInput from '@/components/ui/BaseInput.vue'
 import ValidationError from '@/components/ui/ValidationError.vue'
@@ -45,6 +114,7 @@ export default {
     Field,
     AuthLayout,
     IconArtRegister,
+    IconChecked,
     BaseLabel,
     BaseInput,
     ValidationError
