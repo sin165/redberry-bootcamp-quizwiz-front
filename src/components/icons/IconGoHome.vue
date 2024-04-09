3a0dd0ea3708d4efc2219a387ce86338b2f31051
Tamar Sinauridze <sin165165@gmail.com>
2024-04-09 20:30:38 +0400

    feat: add 404 page

---
+++ src/components/icons/IconGoHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg width="27" height="17" viewBox="0 0 27 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M2.69922 5.94926L21.1444 5.94926C23.4413 5.94926 25.3034 7.81132 25.3034 10.1083L25.3034 11.6339C25.3034 13.9309 23.4413 15.793 21.1444 15.793L6.70964 15.793"
+      stroke="#4B69FD"
+      stroke-width="2.08134"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6.34375 1.57422C4.63521 3.28276 3.67729 4.24068 1.96875 5.94922L6.34375 10.3242"
+      stroke="#4B69FD"
+      stroke-width="2.08134"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
 import PasswordResetView from '@/views/PasswordResetView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/reset-password',
       name: 'reset-password',
       component: PasswordResetView
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
