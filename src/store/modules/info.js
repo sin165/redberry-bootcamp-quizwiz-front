53f3c1eb922b8f1f481a5d8acc6658d6e3d4cc4a
Tamar Sinauridze <sin165165@gmail.com>
2024-05-10 16:09:55 +0400

    feat: make footer information dynamic

--- src/store/modules/info.js
+++ src/store/modules/info.js
@@ -2,11 +2,21 @@ export default {
   namespaced: true,
   state() {
     return {
+      email: '',
+      telephone: '',
+      facebook: '',
+      linkedin: '',
       quizCount: 0,
       categoryCount: 0
     }
   },
   mutations: {
+    setContactInfo(state, payload) {
+      state.email = payload.email
+      state.telephone = payload.telephone
+      state.facebook = payload.facebook
+      state.linkedin = payload.linkedin
+    },
     setQuizCount(state, payload) {
       state.quizCount = payload
     },
@@ -15,6 +25,9 @@ export default {
     }
   },
   actions: {
+    setContactInfo(context, payload) {
+      context.commit('setContactInfo', payload)
+    },
     setQuizCount(context, payload) {
       context.commit('setQuizCount', payload)
     },
@@ -23,6 +36,18 @@ export default {
     }
   },
   getters: {
+    email(state) {
+      return state.email
+    },
+    telephone(state) {
+      return state.telephone
+    },
+    facebook(state) {
+      return state.facebook
+    },
+    linkedin(state) {
+      return state.linkedin
+    },
     quizCount(state) {
       return state.quizCount
     },
